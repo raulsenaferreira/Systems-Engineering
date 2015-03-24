70cf959f05a78770121f5b41793efcd48cafbc9a
Raul Sena Ferreira <raulsenaferreira@gmail.com>
2015-03-24 20:53:39 -0300

    removing old files

--- Estrutura de Dados e Algoritmos/notacaoPolonesaReversa.cpp
+++ Estrutura de Dados e Algoritmos/notacaoPolonesaReversa.cpp
@@ -11,40 +11,42 @@ void imprimePilha(char vet[], int size){
 
 int main(int argc, char const *argv[]){
 	string expressao;
-	char pilhaNum[200];
-	char pilhaOp[200];
 
 	while(getline(cin, expressao)){
 		int i = 0;
-		int contP=0;
 		int contN=0;
 		int topoNum=0;
 		int topoOp=0;
+		char pilhaNum[200];
+		char pilhaOp[200];
 
 		while(expressao[i]!='\0'){
 			if(expressao[i]=='+' || expressao[i]=='-' || expressao[i]=='*' || expressao[i]=='/'){
 				pilhaOp[topoOp++]=expressao[i];
 			}
 			else if (expressao[i]=='('){
-				contP++;
 				contN=0;
+				if(contN>=2){
+					pilhaNum[topoNum++]=pilhaOp[--topoOp];
+					contN=0;
+				}
 			}
 			else if(expressao[i]==')'){
 				pilhaNum[topoNum++]=pilhaOp[--topoOp];
-				contP--;
 			}
 			else{
 				pilhaNum[topoNum++]=expressao[i];
 				contN++;
-			}
-
-			if(expressao[i]=='\0'){
-
-				while(topoOp!=0){
+				if(contN>=2 && pilhaOp[topoOp-1]=='*'){
 					pilhaNum[topoNum++]=pilhaOp[--topoOp];
+					contN=0;
 				}
 			}
+
 			i++;
+		}
+		while(topoOp>0){
+			pilhaNum[topoNum++]=pilhaOp[--topoOp];
 		}
 
 		imprimePilha(pilhaNum, topoNum);
