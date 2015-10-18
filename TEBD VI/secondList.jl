2ad704424774aae30f2c30c9279bd0b9ab56fc93
raul <raulsenaferreira@gmail.com>
2015-10-18 12:34:13 -0200

    Second list almost done.

--- TEBD VI/secondList.jl
+++ TEBD VI/secondList.jl
@@ -1,7 +1,7 @@
 using Gadfly
 
 #Question 1
-array = readdlm("path/u.data")
+array = readdlm("/home/raul/workspace/u.data")
 qtyPerUser = hcat(hist(array[:,1], 0.1:943.9)[2], 1:943)
 qtyPerUser = sortrows(qtyPerUser,by=x->(x[1]), rev=true)
 plot(x=qtyPerUser[:,2], y=qtyPerUser[:,1], Geom.line)
@@ -14,5 +14,14 @@ numLines=size(array, 1)
 randomBase = array[randperm(numLines), :]
 training = randomBase[1:(numLines*0.8), :]
 test = randomBase[(numLines*0.8+1):end, :]
+globalMean = mean(training[:,3])
+userAverages = [mean(training[find(x->(x==i), training[:,1]), 3]) for i=1:943]
+MAE = [mean(abs(userAverages[i]-test[find(x->(x==i), test[:,1]), 3])) for i=1:943]
 
-globalMean = mean(randomBase[:,3])
+#Question 4
+globalMean = mean(training[!isnan(training[:,3])])
+itemAverages = [mean(training[find(x->(x==i), training[:,2]), 3]) for i=1:1682]
+itemAverages[find(x->(isnan(x)), itemAverages)]=globalMean
+MAE = [mean(abs(itemAverages[i]-test[find(x->(x==i), test[:,2]), 3])) for i=1:1682]
+
+#Question 5
