76724775804d83603238f16c15e1ccac9eba40de
raul <raulsenaferreira@gmail.com>
2015-10-23 08:31:25 -0200

    updating

--- TEBD VI/secondList.jl
+++ TEBD VI/secondList.jl
@@ -1,7 +1,7 @@
 using Gadfly
 
 #Question 1
-array = readdlm("/home/raul/workspace/u.data")
+array = readdlm("u.data")
 qtyPerUser = hcat(hist(array[:,1], 0.1:943.9)[2], 1:943)
 qtyPerUser = sortrows(qtyPerUser,by=x->(x[1]), rev=true)
 plot(x=qtyPerUser[:,2], y=qtyPerUser[:,1], Geom.line)
@@ -17,12 +17,9 @@ randomBase = array[randperm(numLines), :]
 training = randomBase[1:(numLines*0.8), :]
 test = randomBase[(numLines*0.8+1):end, :]
 
+#media dos ratings
 globalMean = mean(training[:,3])
 userAverages = [mean(training[find(x->(x==i), training[:,1]), 3]) for i=1:943]
-
-#mean of averages
-#MAE = [mean(abs(userAverages[i]-test[find(x->(x==i), test[:,1]), 3])) for i=1:943]
-#print("MAE = ", mean(MAE))
 
 #total mean
 errors = [abs(userAverages[i]-test[find(x->(x==i), test[:,1]), 3]) for i=1:943]
@@ -34,10 +31,20 @@ print("MAE = ", mean(err[2:end]))
 #Question 4
 itemAverages = [mean(training[find(x->(x==i), training[:,2]), 3]) for i=1:1682]
 itemAverages[find(x->(isnan(x)), itemAverages)]=globalMean
-MAE = [abs(itemAverages[i]-test[find(x->(x==i), test[:,2]), 3]) for i=1:1682]
+errors = [abs(itemAverages[i]-test[find(x->(x==i), test[:,2]), 3]) for i=1:1682]
 err = zeros(1)
-for e in MAE  append!(err, e) end
+for e in errors  append!(err, e) end
 print("MAE = ", mean(err[2:end]))
 
 
 #Question 5
+#array = readdlm("u.item", '|')
+itemAverages = [mean(training[find(x->(x==i), training[:,2]), 3]) for i=1:1682]
+#media dos itens desconsiderando os NaN
+globalMeanItem = mean(itemAverages[find(x->(!isnan(x)), itemAverages)])
+
+itemAverages[find(x->(isnan(x)), itemAverages)]=globalMeanItem
+errors = [abs(itemAverages[i]-test[find(x->(x==i), test[:,2]), 3]) for i=1:1682]
+err = zeros(1)
+for e in errors  append!(err, e) end
+print("MAE = ", mean(err[2:end]))
