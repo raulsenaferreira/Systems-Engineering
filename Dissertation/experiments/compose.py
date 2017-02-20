035fbd8d2550d787fa7d7bb1a437268296177f3a
Raul Sena Ferreira <raulsf@cos.ufrj.br>
2017-02-20 10:46:42 -0300

    Update compose.py

--- Dissertation/experiments/compose.py
+++ Dissertation/experiments/compose.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn import mixture
 from sklearn.neighbors.kde import KernelDensity
 from sklearn.cluster import KMeans
-
+from sklearn import svm
 '''
 Eight  features  (temperature,  dew  point,
 sea-level  pressure,  visibility,  average  wind  speed,  max  sus-
@@ -12,13 +12,16 @@ tained wind speed, and minimum and maximum temperature)
 are  used  to  determine  whether  each  day  experienced  rain  or
 no rain.
 '''
-data = pd.read_csv('C:\\Users\\user\\Documents\\Dissertacao\\gsod_2017\\007026-99999-2017.op',sep = "\t")
-print(data)
 n_samples = 30#300
-
-# generate random sample, two components
+n_dimensions = 3
+# generate random sample
 np.random.seed(0)
 
+#Test 1 with single distribution
+randd = np.random.randn(n_samples, n_dimensions)
+
+'''
+#Test 2 with two different distributions
 # generate spherical data centered on (20, 20)
 shifted_gaussian = np.random.randn(n_samples, 2) + np.array([20, 20])
 
@@ -26,28 +29,33 @@ shifted_gaussian = np.random.randn(n_samples, 2) + np.array([20, 20])
 C = np.array([[0., -0.7], [3.5, .7]])
 stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C)
 
-#Test
-randd = np.random.randn(10, 3)
-
 # concatenate the two datasets into the final training set
 #X_train = np.vstack([shifted_gaussian, stretched_gaussian])
-X_train = np.vstack(randd)
+'''
 
-#K-means
-kmeans = KMeans(n_clusters=2).fit(X_train)
-clusters = kmeans.labels_
+X_train = randd
 
-#Slicing instances according to their inferred clusters
 label1 = 0
 label2 = 1
-trainClasses = {}
-trainClasses[label1]=[X_train[i] for i in range(0,len(clusters)) if clusters[i] == label1]
-trainClasses[label2]=[X_train[i] for i in range(0,len(clusters)) if clusters[i] == label2]
+classes=[label1, label2]
+numClasses = len(classes)
 
-# fit a Gaussian Mixture Model with two components
+
+#K-means
+kmeans = KMeans(n_clusters=numClasses).fit(X_train)
+clusters = kmeans.labels_
+#predictedClusters = kmeans.predict([[0, 0, 0], [4, 4, 5]]) #predciting
+
+#Slicing instances according to their inferred clusters
+trainClasses = {}
+for c in range(numClasses):
+    trainClasses[classes[c]]=[X_train[i] for i in range(len(clusters)) if clusters[i] == c]
+    
+
+# fit a Gaussian Mixture Model with n components
 gmm = {}
 for c, points in trainClasses.items():
-    clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
+    clf = mixture.GaussianMixture(n_components=6, covariance_type='full')
     clf.fit(points)
     gmm[c] = clf.score_samples(points)
 
@@ -57,11 +65,24 @@ for c, points in trainClasses.items():
     kernel = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(points)
     kde[c] = kernel.score_samples(points)
 
-#print("Original data: ",X_train, "\n")
-#print("K-Means clusters: ", clusters, "\n")
-#print("Points from Cluster", label1, ": ", trainClasses[label1], "\n")
-#print("Points from Cluster", label2, ": ", trainClasses[label2], "\n")
-#print("PDF for class", label1, "using GMM: ", gmm[label1], "\n")
-#print("PDF for class", label2, "using GMM: ", gmm[label2], "\n")
-#print("PDF for class", label1, "using KDE: ", kde[label1], "\n")
-#print("PDF for class", label2, "using KDE: ", kde[label2], "\n")
+print("Original data: ",X_train, "\n")
+print("K-Means clusters: ", clusters, "\n")
+print("Points from Cluster", label1, ": ", trainClasses[label1], "\n")
+print("Points from Cluster", label2, ": ", trainClasses[label2], "\n")
+print("PDF for class", label1, "using GMM: ", gmm[label1], "\n")
+print("PDF for class", label2, "using GMM: ", gmm[label2], "\n")
+print("PDF for class", label1, "using KDE: ", kde[label1], "\n")
+print("PDF for class", label2, "using KDE: ", kde[label2], "\n")
+
+#Comparing the results of entire method above with SVM
+X = X_train
+y = kmeans.labels_ #putting kmeans labels as a response from specialist (yes, i'm lazy)
+clf = svm.SVC()
+clf.fit(X, y)
+svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    tol=0.001, verbose=False)
+
+
+print("Sample predicted by SVM: ", clf.predict([[0, 0, 0], [4, 4, 5]]))
