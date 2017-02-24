bdab283024e1ad10532522fcea87614a13542214
Raul Sena Ferreira <raulsf@cos.ufrj.br>
2017-02-24 12:29:44 -0300

    Update testingPipeline.py

--- Dissertation/experiments/testingPipeline.py
+++ Dissertation/experiments/testingPipeline.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,88 +6,112 @@ from sklearn import mixture
 from sklearn.neighbors.kde import KernelDensity
 from sklearn.cluster import KMeans
 from sklearn import svm
-'''
-Eight  features  (temperature,  dew  point,
-sea-level  pressure,  visibility,  average  wind  speed,  max  sus-
-tained wind speed, and minimum and maximum temperature)
-are  used  to  determine  whether  each  day  experienced  rain  or
-no rain.
-'''
-#Example of reading NOAA dataset with pandas
-#data = pd.read_csv('C:\\Users\\user\\Documents\\Dissertacao\\gsod_2017\\007026-99999-2017.op',sep = "\t")
-#print(data)
 
-n_samples = 30#300
-n_dimensions = 3
-# generate random sample
-np.random.seed(0)
-
-#Test 1 with single distribution
-randd = np.random.randn(n_samples, n_dimensions)
-
-'''
-#Test 2 with two different distributions
-# generate spherical data centered on (20, 20)
-shifted_gaussian = np.random.randn(n_samples, 2) + np.array([20, 20])
-
-# generate zero centered stretched Gaussian data
-C = np.array([[0., -0.7], [3.5, .7]])
-stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C)
-
-# concatenate the two datasets into the final training set
-#X_train = np.vstack([shifted_gaussian, stretched_gaussian])
-'''
-
-X_train = randd
-
-label1 = 0
-label2 = 1
-classes=[label1, label2]
-numClasses = len(classes)
+def kMeans(X, classes):  
+    numClasses = len(classes)
+    kmeans = KMeans(n_clusters=numClasses).fit(X)
+    
+    return kmeans
 
 
-#K-means
-kmeans = KMeans(n_clusters=numClasses).fit(X_train)
-clusters = kmeans.labels_
-#predictedClusters = kmeans.predict([[0, 0, 0], [4, 4, 5]]) #predciting
-
-#Slicing instances according to their inferred clusters
-trainClasses = {}
-for c in range(numClasses):
-    trainClasses[classes[c]]=[X_train[i] for i in range(len(clusters)) if clusters[i] == c]
+def svm(X, y):
+    clf = svm.SVC()
+    clf.fit(X, y)
+    
+    svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+        decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+        max_iter=-1, probability=False, random_state=None, shrinking=True,
+        tol=0.001, verbose=False)
+    
+    return SVM
     
 
-# fit a Gaussian Mixture Model with n components
-gmm = {}
-for c, points in trainClasses.items():
-    clf = mixture.GaussianMixture(n_components=6, covariance_type='full')
-    clf.fit(points)
-    gmm[c] = clf.score_samples(points)
-
-#Kernel density estimation
-kde={}
-for c, points in trainClasses.items():
-    kernel = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(points)
-    kde[c] = kernel.score_samples(points)
-
-print("Original data: ",X_train, "\n")
-print("K-Means clusters: ", clusters, "\n")
-print("Points from Cluster", label1, ": ", trainClasses[label1], "\n")
-print("Points from Cluster", label2, ": ", trainClasses[label2], "\n")
-print("PDF for class", label1, "using GMM: ", gmm[label1], "\n")
-print("PDF for class", label2, "using GMM: ", gmm[label2], "\n")
-print("PDF for class", label1, "using KDE: ", kde[label1], "\n")
-print("PDF for class", label2, "using KDE: ", kde[label2], "\n")
-
-#Comparing the results of entire method above with SVM
-X = X_train
-y = kmeans.labels_ #putting kmeans labels as a response from specialist (yes, i'm lazy)
-clf = svm.SVC()
-clf.fit(X, y)
-svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
-    max_iter=-1, probability=False, random_state=None, shrinking=True,
-    tol=0.001, verbose=False)
+def gmm(instances):
+    gmm = {}
+    for c, points in instances.items():
+        clf = mixture.GaussianMixture(n_components=6, covariance_type='full')
+        clf.fit(points)
+        gmm[c] = clf.score_samples(points)
+    return gmm
 
 
-print("Sample predicted by SVM: ", clf.predict([[0, 0, 0], [4, 4, 5]]))
+def kde(instances):
+    kde={}
+    for c, points in instances.items():
+        kernel = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(points)
+        kde[c] = kernel.score_samples(points)
+    return kde
+
+
+def baseClassifier(instancesToPredict, classifier):
+    return classifier.predict(instancesToPredict)
+
+
+#Slicing instances according to their inferred clusters
+def slicingClusteredData(X_train, clusters, classes):
+    instances = {}
+    for c in range(numClasses):
+        instances[classes[c]]=[X_train[i] for i in range(len(clusters)) if clusters[i] == c]
+    
+    return instances
+
+
+#Cutting data for next iteration
+def compactingDataDensityBased(data, instances, criteria):
+    
+    return selectedInstances
+    
+    
+def main():
+    #current directory
+    path = os.getcwd() 
+
+    '''
+    Reading NOAA dataset:
+    Eight  features  (average temperature, minimum temperature, maximum temperature, dew
+    point,  sea  level  pressure,  visibility,  average wind speed, maximum  wind  speed)
+    are  used  to  determine  whether  each  day  experienced  rain  or no rain.
+    '''
+    dataValues = pd.read_csv(path+'\\noaa_data.csv',sep = ",")
+    dataLabels = pd.read_csv(path+'\\noaa_label.csv',sep = ",")
+
+
+    #Test 0: Predicting 10 instances
+    unlabeledData = dataValues[initialDataLength:initialDataLength+10]
+
+    # ***** Box 0 *****
+    #starting labeled data with 5%
+    initialDataLength = round((0.05)*len(dataValues))
+    X_train = np.hstack((dataValues[:initialDataLength].as_matrix(), dataLabels[:initialDataLength].as_matrix()))
+
+    #Starting the process
+    for t in range(len(unlabeledData)):
+        print("Step ",t)
+        
+        # ***** Box 1 *****
+        Ut = unlabeledData[t]
+        classes=[0, 1]
+
+        # ***** Box 2 *****
+        kmeans = kMeans(X_train, classes)
+        clusters = kmeans.labels_
+        predicted = baseClassifier(Ut, kmeans)
+
+        instances = slicingClusteredData(X_train, np.vstack([clusters, predicted]), classes)
+
+        # ***** Box 3 *****
+        #Testing with two different methods
+        gmm = gmm(instances)
+        #kde = kde(instances)
+        
+        # ***** Box 4 *****
+        instancesGMM = compactingDataDensityBased(instances, gmm, criteria)
+        #instancesKDE = compactingDataDensityBased(instances, kde, criteria)
+        
+        # ***** Box 5 *****
+        X_train = instancesGMM
+        #X_train = instancesKDE
+        print("Selected data: ", X_train)
+        
+        
+main()
