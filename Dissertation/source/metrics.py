8b867a45073efac79bd94c888da4d1453bfb7625
raulsenaferreira <raulsenaferreira@gmail.com>
2017-08-27 23:45:08 -0300

    adding new metrics

--- Dissertation/source/metrics.py
+++ Dissertation/source/metrics.py
@@ -3,6 +3,8 @@ import sys
 sys.path.append(os.getcwd()+"\\source")
 
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import f1_score
 from math import sqrt
 import numpy as np
 from source.plotFunctions import plotAccuracy
@@ -11,6 +13,22 @@ from source.plotFunctions import plotAccuracy
 def evaluate(y_actual, y_predicted):
     return accuracy_score(y_actual, y_predicted)*100
     
+
+def F1(y_true, y_pred):
+	return f1_score(y_true, y_pred, average=None)
+
+
+def macroF1(y_true, y_pred):
+	return f1_score(y_true, y_pred, average='macro')
+
+
+def microF1(y_true, y_pred):
+	return f1_score(y_true, y_pred, average='micro')
+
+
+def mcc(Y_true, y_predicted):
+	return matthews_corrcoef(y_true, y_predicted)
+
 
 def finalEvaluation(arrAcc, batches):
     print("Average Accuracy: ", np.mean(arrAcc))
