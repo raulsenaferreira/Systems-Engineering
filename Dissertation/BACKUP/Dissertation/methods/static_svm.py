7a60dfb871bca3fad418bd113da4792b990429b5
raulsenaferreira <raulsenaferreira@gmail.com>
2017-10-10 15:04:35 -0300

    BACKUP

---
+++ Dissertation/BACKUP/Dissertation/methods/static_svm.py
@@ -0,0 +1,32 @@
+from source import classifiers
+from source import metrics
+from source import util
+
+
+def start(dataValues, dataLabels, **kwargs):
+    initialLabeledDataPerc = kwargs["initialLabeledDataPerc"]
+    usePCA = kwargs["usePCA"]
+    batches = kwargs["batches"]
+    sizeOfBatch = kwargs["sizeOfBatch"]
+
+    print("METHOD: SVM as classifier (STATIC)")
+
+    arrAcc = []
+    initialDataLength = 0
+    finalDataLength = round((initialLabeledDataPerc)*sizeOfBatch)
+    
+    #Initial labeled data
+    X, y = util.loadLabeledData(dataValues, dataLabels, initialDataLength, finalDataLength, usePCA)
+    clf = classifiers.svmClassifier(X, y)
+    initialDataLength=finalDataLength
+    finalDataLength=sizeOfBatch
+    
+    for t in range(batches):
+        Ut, yt = util.loadLabeledData(dataValues, dataLabels, initialDataLength, finalDataLength, usePCA)
+        predicted = clf.predict(Ut)
+        arrAcc.append(metrics.evaluate(yt, predicted))
+        
+        initialDataLength=finalDataLength
+        finalDataLength+=sizeOfBatch
+    
+    return "Static SVM", arrAcc, X, y
